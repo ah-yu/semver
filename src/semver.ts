234d5c3ef286526413917365c8f8903351ad08ed
buji <zheyuzhang03@gmail.com>
2019-06-19 01:07:23 +0800

    remove useless code

--- src/semver.ts
+++ src/semver.ts
@@ -1,34 +1,15 @@
-import {
-  SemVer as SemVerBase,
-  // eslint-disable-next-line no-unused-vars
-  Options as SemVerOptions,
-  minor
-} from 'semver';
-
-export function prod(version: string | SemVer): boolean {
-  if (typeof version === 'string') {
-    version = new SemVer(version);
-  }
-  return version.isProd;
-}
-
-export function dev(version: string): boolean {
-  return !prod(version);
-}
+import { SemVer as SemVerBase, Options, minor } from 'semver';
 
 export class SemVer extends SemVerBase {
   public isProd: boolean;
   public isDev: boolean;
 
-  constructor(
-    version: string | SemVer,
-    optionsOrLoose?: boolean | SemVerOptions
-  ) {
+  constructor(version: string | SemVer, optionsOrLoose?: boolean | Options) {
     super(version, optionsOrLoose);
 
     const minorVersion = minor(version);
 
-    this.isProd = minorVersion % 2 === 0; // production release
+    this.isProd = minorVersion % 2 === 0;
     this.isDev = !this.isProd;
   }
 }
